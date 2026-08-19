a7d014f1285c1aed00925fae7df29e10200c6aff
cookedRay03 <wirapratama020304@gmail.com>
2026-08-20 03:33:40 +0700

    add module 1 code 1

---
+++ module1/code1.tsx
@@ -0,0 +1,54 @@
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#e0f2fe",
+    justifyContent: "center",
+    padding: 20,
+  },
+
+  card: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 16,
+    alignItems: "center",
+  },
+
+  icon: {
+    marginBottom: 12,
+  },
+
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "red",
+    marginBottom: 20,
+    textAlign: "center",
+  },
+
+  input: {
+    borderWidth: 2,
+    borderColor: "blue",
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 20,
+    width: "100%",
+  },
+
+  button: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 8,
+    backgroundColor: "#2563eb",
+    padding: 12,
+    borderRadius: 10,
+    width: "100%",
+  },
+
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
